bc5ebda754325aedbe9d9df9f1822f553ba4909d
Dan P. Smith <mrdanpsmith@gmail.com>
2026-08-13 00:55:56 -0700

    Switch to cobertura reports.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     exclude: ['**/node_modules/**', '**/.cache/**', '**/dist/**'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov'],
+      reporter: ['text', 'lcov', 'cobertura'],
       include: ['src/**'],
     },
   },
